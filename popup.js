fbb525316cefaa93bf5fe4c080246ce6d3448009
badal11 <badaltripathi1@gmail.com>
2023-07-19 10:41:52 +0545

    Delete All button at the top

--- popup.js
+++ popup.js
@@ -1,10 +1,19 @@
 // Popup script
 chrome.runtime.sendMessage({ type: "getWords" }, function(response) {
   var wordList = document.getElementById("wordList");
-  
+  var deleteAllBtn = document.getElementById("deleteAllBtn");
+
   // Reverse the array of words to display newest first
   response.words.reverse();
-  
+
+  // Add event listener to the Delete All button
+  deleteAllBtn.addEventListener("click", function() {
+    chrome.storage.local.set({ words: [] }, function() {
+      wordList.innerHTML = ""; // Clear the word list
+      console.log("All words deleted.");
+    });
+  });
+
   response.words.forEach(function(item) {
     var li = document.createElement("li");
     var word = document.createElement("span");
