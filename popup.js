03a01a0a95781b4563e89ef6be9eea5eac6672c7
badal11 <badaltripathi1@gmail.com>
2023-07-19 10:24:09 +0545

    highlighting in bold

--- popup.js
+++ popup.js
@@ -6,7 +6,12 @@ chrome.runtime.sendMessage({ type: "getWords" }, function(response) {
     var word = document.createElement("span");
     var sentence = document.createElement("span");
     word.textContent = item.word;
+    word.classList.add("highlight");
     sentence.innerHTML = item.sentence;
+    sentence.innerHTML = sentence.innerHTML.replace(
+      new RegExp(`\\b${item.word}\\b`, "gi"),
+      '<span class="highlight">$&</span>'
+    );
     li.appendChild(word);
     li.innerHTML += ": ";
     li.appendChild(sentence);
